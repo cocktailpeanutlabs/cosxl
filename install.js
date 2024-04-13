6ac9cc817e8d1992380868d7b7d057f0c74f93c8
cocktailpeanut <cocktailpeanuts@proton.me>
2024-04-13 01:00:04 -0400

    init

---
+++ install.js
@@ -0,0 +1,40 @@
+module.exports = {
+  run: [{
+    method: "shell.run",
+    params: {
+      message: [
+        "git clone https://huggingface.co/spaces/cocktailpeanut/cosxl app",
+      ]
+    }
+  }, {
+    method: "script.start",
+    params: {
+      uri: "torch.js",
+      params: {
+        path: "app",
+        venv: "env",
+//        xformers: true
+      }
+    }
+  }, {
+    method: "shell.run",
+    params: {
+      venv: "env",
+      path: "app",
+      message: [
+        "pip install gradio",
+        "pip install -r requirements.txt",
+      ]
+    }
+  }, {
+    method: "fs.share",
+    params: {
+      venv: "app/env"
+    }
+  }, {
+    method: "notify",
+    params: {
+      html: "Click the 'start' tab to get started!"
+    }
+  }]
+}
